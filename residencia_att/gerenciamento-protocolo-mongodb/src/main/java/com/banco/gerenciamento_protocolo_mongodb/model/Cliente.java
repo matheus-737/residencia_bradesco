af615ef338d3b1cf1e695ff22ec0fa1a64fd3f93
vinicius <viniciussantana2003@hotmail.com>
2024-11-06 11:23:41 -0300

    Adicionar campo Telefone no modelo Cliente

--- residencia_att/gerenciamento-protocolo-mongodb/src/main/java/com/banco/gerenciamento_protocolo_mongodb/model/Cliente.java
+++ residencia_att/gerenciamento-protocolo-mongodb/src/main/java/com/banco/gerenciamento_protocolo_mongodb/model/Cliente.java
@@ -10,12 +10,14 @@ public class Cliente {
     private String id;
     private String email;
     private TipoCliente tipoCliente;
+    private Telefone telefone;
 
     public Cliente() {}
 
-    public Cliente(String email, TipoCliente tipoCliente) {
+    public Cliente(String email, TipoCliente tipoCliente, Telefone telefone) {
         this.email = email;
         this.tipoCliente = tipoCliente;
+        this.telefone = telefone;
     }
 
     // Getters e Setters
@@ -44,10 +46,68 @@ public class Cliente {
         this.tipoCliente = tipoCliente;
     }
 
+    public Telefone getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
+
     // Enum TipoCliente
 
     public enum TipoCliente {
         FISICO,
         JURIDICO
+    }
+
+    // Classe interna Telefone
+
+    public static class Telefone {
+        private String numero;
+        private String ddd;
+        private TipoTelefone tipoTelefone;
+
+        public Telefone() {}
+
+        public Telefone(String numero, String ddd, TipoTelefone tipoTelefone) {
+            this.numero = numero;
+            this.ddd = ddd;
+            this.tipoTelefone = tipoTelefone;
+        }
+
+        // Getters e Setters para Telefone
+
+        public String getNumero() {
+            return numero;
+        }
+
+        public void setNumero(String numero) {
+            this.numero = numero;
+        }
+
+        public String getDdd() {
+            return ddd;
+        }
+
+        public void setDdd(String ddd) {
+            this.ddd = ddd;
+        }
+
+        public TipoTelefone getTipoTelefone() {
+            return tipoTelefone;
+        }
+
+        public void setTipoTelefone(TipoTelefone tipoTelefone) {
+            this.tipoTelefone = tipoTelefone;
+        }
+    }
+
+    // Enum TipoTelefone
+
+    public enum TipoTelefone {
+        RESIDENCIAL,
+        COMERCIAL,
+        CELULAR
     }
 }
